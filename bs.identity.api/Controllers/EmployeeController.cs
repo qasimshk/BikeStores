8ec887d291db599405265fd4a18df30212f35429
Qasim <ins_sheikh@hotmail.com>
2021-02-24 09:13:23 +0000

    work in progress

--- bs.identity.api/Controllers/EmployeeController.cs
+++ bs.identity.api/Controllers/EmployeeController.cs
@@ -4,12 +4,15 @@ using bs.identity.application.Commands.EmployeeRegistration;
 using bs.identity.domain.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 
 namespace bs.identity.api.Controllers
 {
     public class EmployeeController : BaseController
     {
         [HttpGet("all")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
         public IActionResult All()
         {
             // add paging to it
@@ -17,12 +20,16 @@ namespace bs.identity.api.Controllers
         }
 
         [HttpGet("{employeeId}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
         public IActionResult Employee(Guid employeeId)
         {
             return Ok();
         }
         
         [HttpGet("search")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
         public IActionResult Employee([FromQuery] EmployeeFilter filter)
         {
             // in store case filter how many employee are working in the perticular store
@@ -30,12 +37,17 @@ namespace bs.identity.api.Controllers
         }
 
         [HttpPost("register")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
         public async Task<IActionResult> Register([FromBody] EmployeeRegisterRequestDto request)
         {
             return Ok(await _mediator.Send(_mapper.Map<EmployeeRegistrationCommand>(request)));
         }
 
         [HttpPut("{employeeId}/update")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
         public IActionResult EmployeeUpdate([FromRoute] Guid employeeId)
         {
             return Ok();
--- bs.identity.api/Controllers/TokenController.cs
+++ bs.identity.api/Controllers/TokenController.cs
@@ -2,6 +2,7 @@
 using bs.identity.application.Commands.TokenAuthenticate;
 using bs.identity.application.Commands.TokenRefresh;
 using bs.identity.domain.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System.Net;
 using System.Threading.Tasks;
@@ -12,6 +13,8 @@ namespace bs.identity.api.Controllers
     {
         [HttpPost("authenticate")]
         [ProducesResponseType(typeof(UserLoginResponseDto), (int)HttpStatusCode.OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
         public async Task<IActionResult> Authorize([FromBody] UserLoginRequestDto request)
         {
             return Ok(await _mediator.Send(new TokenAuthenticateCommand(request)));
@@ -19,6 +22,7 @@ namespace bs.identity.api.Controllers
 
         [HttpPost("refresh")]
         [ProducesResponseType(typeof(UserLoginResponseDto), (int)HttpStatusCode.OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
         public async Task<IActionResult> Refresh([FromBody] string refreshToken)
         {
             return Ok(await _mediator.Send(new TokenRefreshCommand(refreshToken)));
